658bab47bf03cc86602d100cb8fe4cce06e0176d
DrTheodor <DrTheodor@users.noreply.github.com>
2024-06-20 17:03:40 +0300

    i hate js part 5

--- src/util.js
+++ src/util.js
@@ -2,15 +2,7 @@ const fs = require('fs')
 
 function flatFiles(file) {
     if (file.endsWith('*')) {
-        let paths = []
-
-        fs.readdir(file.slice(0, file.length - 1), (err, files) => {
-            files.forEach(f => {
-                paths.push(f);
-            })
-        })
-
-        return paths
+        return fs.readdirSync(file.slice(0, file.length - 1));
     }
 
     return [file]
