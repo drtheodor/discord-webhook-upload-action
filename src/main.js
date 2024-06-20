eb3a7d81e54d838747058aa9c6c1d7eed0c96a63
DrTheodor <DrTheodor@users.noreply.github.com>
2024-06-20 15:41:57 +0300

    e

---
+++ src/main.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core')
+const { Webhook } = require('discord-webhook-node')
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+async function run() {
+  try {
+    const url = core.getInput('url')
+    const username = core.getInput('username')
+    const avatar = core.getInput('avatar')
+
+    const file = core.getInput('file')
+
+    const hook = new Webhook(url)
+
+    hook.setUsername(username)
+    hook.setAvatar(avatar)
+
+    if (file.endsWith('*')) {
+      fs.readdir(file, (err, files) => {
+        files.forEach(async f => {
+          core.debug(`Sent file from ${file}: ${f}`)
+          await hook.sendFile(f)
+        })
+      })
+
+      return
+    }
+
+    core.debug(`Sent ${file}`)
+    await hook.sendFile(file)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+module.exports = {
+  run
+}
