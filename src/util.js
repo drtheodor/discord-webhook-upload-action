e966ade275617c5d184e5bcc846d5d3c5e6f32fd
DrTheodor <DrTheodor@users.noreply.github.com>
2024-06-20 17:07:18 +0300

    i hate js part 6

--- src/util.js
+++ src/util.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 
 function flatFiles(file) {
     if (file.endsWith('*')) {
-        return fs.readdirSync(file.slice(0, file.length - 1));
+        const sliced = file.slice(0, file.length - 1)
+        return fs.readdirSync(sliced).flatMap((f) => sliced + '/' + f);
     }
 
     return [file]
