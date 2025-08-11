a7796e48aedaca4f9a3f286eff6501522cc5ad56
Theo <git@theo.is-a.dev>
2025-08-11 17:04:35 +0700

    no .d.ts

--- dist/search.d.ts
+++
@@ -1,6 +0,0 @@
-/** Shamelessly copied from https://github.com/actions/upload-artifact/blob/main/src/shared/search.ts */
-export interface SearchResult {
-    filesToUpload: string[];
-    rootDirectory: string;
-}
-export declare function findFilesToUpload(searchPath: string, includeHiddenFiles?: boolean): Promise<SearchResult>;
--- dist/util.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function fmt<T extends Record<string, unknown>>(template: string, values: T): string;
-export interface SplitOptions {
-    maxLength?: number;
-    char?: string;
-    prepend?: string;
-    append?: string;
-}
-export declare function send(url: string, name: string, avatar: string, text: string, file: string, maxLength?: number): Promise<void>;
