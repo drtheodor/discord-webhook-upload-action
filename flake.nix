716b17562586c181bed793096b013b7789563631
Theo <git@theo.is-a.dev>
2026-06-09 15:16:02 +0300

    feat: add a nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem = { config, self', pkgs, lib, system, ... }: let
+        nativeBuildInputs = with pkgs; [
+        ];
+
+        buildInputs = with pkgs; [
+            bun
+            stdenv.cc.cc
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          inherit nativeBuildInputs buildInputs;
+
+          env = {
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
+        };
+      };
+    };
+}
